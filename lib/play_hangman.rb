ab41594da2cb432d3ccfa4c990b95de1f91faed4
Christopher Arbelo <christopherarbelo@gmail.com>
2020-07-20 16:21:52 -0400

    Main driver for game

--- lib/play_hangman.rb
+++ lib/play_hangman.rb
@@ -7,8 +7,17 @@ puts 'Hangman initiated'
 puts 'Rules'
 puts '- You have 5 tries to guess the word correctly'
 puts '- Correct guesses doesn\'t count as a try'
+
+puts 'Save game/Load game'
+puts '- To save game at any time enter \'save\''
+puts '- Any previously saved game will load automatically'
+puts
+
 loop do
+  game.load and game.loaded = true if File.exist? 'memory/saved_game.txt'
   game.play_game
-  print "\nEnter 'play' to keep playing: "
-  break unless gets.chomp.downcase == 'play'
+  print "\nEnter 'play' to keep playing or 'exit' to stop: "
+  input = gets.chomp.downcase
+  exit if input == 'exit'
+  break unless input == 'play'
 end
